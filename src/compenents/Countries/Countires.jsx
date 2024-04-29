531bf59b73873578743d82b5c1332369cdb50977
Wasifur Rahman Efaz <110248681+efaaz@users.noreply.github.com>
2024-04-29 19:26:55 +0600

    link changes

--- src/compenents/Countries/Countires.jsx
+++ src/compenents/Countries/Countires.jsx
@@ -1,29 +1,38 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Country from "../Country/Country";
+import Spinner from "../Loading/Spinner";
+
 
 function Countires() {
+ let  [loading, setLoading] = useState(true);
   const [cardDetails, setCardDetails] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
+      setLoading(true);
       try {
         const res = await fetch(
           "https://server-side-ecru-zeta.vercel.app/countries"
         ); // Absolute path to the public folder
         const data = await res.json();
         setCardDetails(data);
+        setLoading(false);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
     };
     fetchData();
   }, []);
-  return (
+  if(loading){
+    return <><Spinner></Spinner> </>
+  }else{
+      return (
     <div className="grid lg:grid-cols-3 grid-cols-1 max-w-7xl justify-center mx-auto md:grid-cols-2">
       {cardDetails.map((dataItem, index) => (
         <Country key={index} data={dataItem} />
       ))}
     </div>
   );
-}
+}}
+
 
 export default Countires;
